276cedf8acf6cc816e4e4cfd38d7b86528cbc037
RyanJE <RyanJE@users.noreply.github.com>
2020-07-09 04:32:22 +1000

    Monty hall working

--- monty-hall/GDLIIIProblogRep.py
+++ monty-hall/GDLIIIProblogRep.py
@@ -5,6 +5,8 @@ from problog import get_evaluatable
 from random import choice
 from itertools import groupby
 from copy import deepcopy
+from world import World
+from randomWorld import RandomWorld
 
 #This is just a preliminary program to solve Monty Hall, not generalised yet.
 class GDLIIIProblogRep(object):
@@ -13,12 +15,14 @@ class GDLIIIProblogRep(object):
         self._engine = DefaultEngine()
         # For now, just use the written model
         #baseModelFile = generateProblogStringFromGDLIII(program)
-        self._baseModelFile = PrologFile('./model.prob')
+        #self._baseModelFile = PrologFile('./model.prob')
+        self._baseModelFile = PrologFile('./guessmodel.prob')
         self._playerList = []
-        self._kb = self._engine.prepare(self._baseModelFile)
-        self._playerWorlds = self._initialiseKB()
         self._randomIdentifier = Constant(0) #Hardcoded to give the random player a specific constant id as we apply some special rules to the random player
-        self._moveList = {}
+        self._playerWorlds = self._initialiseKB()
+        self._moveList = dict([(i,None) for i in self._playerList])
+        for i in self._playerList:
+            self._moveList[i] = None
         self._completeState = []
         self._currentLegalMoves = self._generateLegalMoves()
         self._step = 1
@@ -41,92 +45,113 @@ class GDLIIIProblogRep(object):
     def _generateLegalMoves(self):
         legalMoves = {}
         for player in self._playerWorlds.keys():
-            legalMoves[player] = \
-            list(map(lambda x: x[0], self._getQueryAsTupleWithProbabilities(
-                [Term('legal',player,Var('_'))],\
-                self._kb, \
-                lambda x: self.extractSingleArg(1,x)\
-            )))
+            legalMoves[player] = set()
+            for world in self._playerWorlds[player]:
+                legalMoves[player] = world.query([Term('legal', player, Var('_'))])
+                break
         return legalMoves
 
     #Assumption, term contains a single argument
     def extractSingleArg(self,nArg, term):
         return term.args[nArg]
 
-    def _regenKb(self, preds):
-        newKb = self._engine.prepare(self._baseModelFile)
-        for i in preds:
-            newKb.add_fact(i)
+    def query(self, player, query):
+        initialResults = []
+        for world in self._playerWorlds[player]:
+            initialResults.append(world.query([Term('knows', player, Term('step', Constant(self._step)), query)]))
+        keys = set([i for d in initialResults for i in d.keys()])
+        queryResults = {}
+        for k in keys:
+            queryResults[k] = 0
+            for d in initialResults:
+                if k in d.keys():
+                    queryResults[k] += d[k]
 
-        return newKb
+        return queryResults
 
-    #Normalise a set of probabilities assuming the probabilities in fluentProbs = 1
     #Private
     def _initialiseKB(self):
+        kb = self._engine.prepare(self._baseModelFile)
         initialState = \
-            self._getQueryAsTupleWithProbabilities([Term('init',Var('_'))],self._kb, lambda x: self.extractSingleArg(0,x))
+            set(map(lambda a: Term('ptrue', a[0].args[0]), self._engine.ground_all(kb, queries=[Term('init',Var('_'))]).get_names()))
         players = \
-            self._getQueryAsTupleWithProbabilities([Term('role',Var('_'))],self._kb, lambda x: self.extractSingleArg(0,x))
+            set(map(lambda a: a[0], self._engine.ground_all(kb, queries=[Term('role',Var('_'))]).get_names()))
         playerWorldState = {}
 
-        for playerNum,_ in players:
+        for playerNum in map(lambda a: a.args[0], players):
             self._playerList.append(playerNum)
-            #Worlds are contstructed in a tuple as (Probability of being true world, set of fluents true in that world)
-            playerWorldState[playerNum] = [(1,set(map(lambda a: (Term('knows', playerNum, \
-                Term('step', Constant(1)),a[0]), a[1]),initialState)))]
-
-        self._completeState = initialState
+            knowledge = map(lambda a: Term('knows', playerNum, Term('step', Constant(1)), a.args[0]), initialState)
+            playerPreds = initialState.union(set(knowledge))
+            #Each player starts with a single initial world
+            if playerNum == self._randomIdentifier:
+                #TODO: Replace with random specific world
+                playerWorldState[playerNum] = [World(self._engine, self._baseModelFile, 1, 1, playerPreds, playerNum)]
+            else:
+                playerWorldState[playerNum] = [World(self._engine, self._baseModelFile, 1, 1, playerPreds, playerNum)]
 
         return playerWorldState
+
+    def applyActionsToModelAndUpdate(self):
+        if (None in self._moveList.values()):
+            raise "Error: Must have submitted moves for all players before proceeding"
+        for i in self._playerList:
+            newWorlds = set()
+            for world in self._playerWorlds[i]:
+                newWorlds = newWorlds.union(world.getSuccessorWorlds(self._moveList))
+            self._playerWorlds[i] = self._normaliseProbability(newWorlds)
+        self._currentLegalMoves = self._generateLegalMoves()
+        self._step += 1
+        self._moveList = dict([(i,None) for i in self._playerList])
 
     def submitAction(self, action, player):
         print(action)
         if ( action not in self._currentLegalMoves[player]):
             raise Exception
         self._moveList[player] = action
-        pass
 
     def getPlayerIds(self):
         return [i for i in self._playerWorlds.keys()]
 
     #Normalise a set of probabilities assuming the probabilities in fluentProbs = 1
     #Private
-    def _normaliseProbability(self,worldProbs):
+    def _normaliseProbability(self,worlds):
         totalprob = 0
-        for prob, world in worldProbs:
-            totalprob += prob
-        normalisedProbs = []
-        for prob, world in worldProbs:
-            normalisedProbs.append((prob/totalprob, world))
-        return normalisedProbs
-    
-    def _getQueryAsTupleWithProbabilities(self, queries, database, function = lambda x: x):
-        return self._getNodesWithWeights(self._relevantGround(queries,database),function)
-    
-    def query(self, queries):
-        return self._getQueryAsTupleWithProbabilities(queries, self._kb)
-    #Private
-    def _relevantGround(self, queries, database):
-        return self._engine.ground_all(database, queries=queries)
+        for w in worlds:
+            totalprob += w._prob
+        for w in worlds:
+            w._prob = w._prob / totalprob
+        return worlds
 
-    def getNormalisedKnowledge(self, knowledge):
-        pass
+    
 
 #Test function to demonstrate playing monty hall with the model
-def playMonty(model):
+def playMonty():
+    model = GDLIIIProblogRep('montyhall.gdliii')
     #Playing sequentially just for demonstration
     for _ in range(3):
-        playerMoves = model.getLegalMovesForPlayer(Constant(1))
-        randomMoves = model.getLegalMovesForPlayer(Constant(0))
-        model.submitAction(playerMoves[0], Constant(1))
+        playerMoves = tuple(model.getLegalMovesForPlayer(Constant(1)))
+        randomMoves = tuple(model.getLegalMovesForPlayer(Constant(0)))
+        model.submitAction(choice(playerMoves), Constant(1))
         model.submitAction(choice(randomMoves), Constant(0))
         model.applyActionsToModelAndUpdate()
-        print(model.getPlayersPossibleWorlds(Constant(1)))
-    result = model.query([Term('goal', Constant(1), Var("_"))])
+    result = model.query(Constant(1),Term('goal', Constant(1), Constant(100)))
     print(result)
-    return 1
 
 
-r = GDLIIIProblogRep('montyhall.gdliii')
-print(r.query([Term('knows', Constant(1), Var('_'), Var('_'))]))
-#playMonty(r)
+#Test function to demonstrate playing monty hall with the model
+def playGuessing():
+    model = GDLIIIProblogRep('guess.gdliii')
+    #Playing sequentially just for demonstration
+    for _ in range(3):
+        playerMoves = tuple(model.getLegalMovesForPlayer(Constant(1)))
+        randomMoves = tuple(model.getLegalMovesForPlayer(Constant(0)))
+        model.submitAction(choice(playerMoves), Constant(1))
+        model.submitAction(choice(randomMoves), Constant(0))
+        model.applyActionsToModelAndUpdate()
+    result = model.query(Constant(1),Term('goal', Constant(1), Constant(100)))
+    print(result)
+
+
+
+#playMonty()
+playGuessing()
--- monty-hall/world.py
+++ monty-hall/world.py
@@ -1,29 +1,94 @@
+from problog.logic import Term, Constant,Var, Clause
+from problog import get_evaluatable
+import itertools
+from functools import reduce
+from operator import mul
 class World(object):
-    def __init__(self, engine, kb, step, prob, preds):
-        self._kb = kb
-        self._engine = engine
-        self._prob = prob
-        self._step = step
-        #List of predicates required to generate this world from the base file
+    def __init__(self, engine, model, step, prob, preds, player, tokens=None):
+        #Set of predicates required to generate this world from the base file
         self._preds = preds
 
-    def _addRule(self, head, body):
-        b = body[0]
-        for term in body[1:]:
-            b = b & term
-        rule = head << b
-        self._preds.append(rule)
-        return self._kb.add_clause(rule)
+        self._engine = engine
+        self._player = player
+        self._baseModel = model
+        self._kb = self._engine.prepare(self._baseModel)
 
-    def _addFact(self, term, probability=None):
-        self._preds.append(term)
-        return self._kb.add_fact(term)
+        for i in preds:
+            self._kb += i
+        
+        self._state = None
+        self._prob = prob
+        self._step = step
+        self._tokens = tokens
 
-    def getSuccessorWorlds(self):
+    #Assumption is that this world is discarded after this function is called
+    def getSuccessorWorlds(self, takenMoves):
+        potentialMovesByOthers = {}
+        for i in takenMoves.keys():
+            self._kb += Term('does', i, takenMoves[i].args[1])
+            if i == self._player:
+                potentialMovesByOthers[i] = {
+                    Term('knowsMove', self._player, Term('step', Constant(self._step)),
+                        Term('does', i, takenMoves[i].args[1])): 1
+                }
+            else:
+                potentialMovesByOthers[i] = self.query(\
+                    [Term('knowsMove', self._player, Term('step', Constant(self._step)), \
+                        Term('does', i, Var('_')))])
+        
+        potentialMoveSequences = \
+            self._processMoveSequences(\
+                itertools.product(*[[(key, val) for (key, val) in potentialMovesByOthers[key].items()] \
+                    for key in potentialMovesByOthers.keys()]))
+
+        tokens = list(self.query([Term('sees', self._player, Var('_'))]).keys())
+        successorWorlds = []
+        for moves, seqProb in potentialMoveSequences:
+            copyKb = self.copy()
+            for m in moves:
+                copyKb += m
+            potentialTokens = list(map(lambda a: a.args[2], self.query([Term('knows', self._player, \
+                Term('step', Constant(self._step)), Term('sees', self._player, Var('_')))],kb=copyKb).keys()))
+            nextState = map(lambda a: Term('ptrue', a.args[0]),\
+                self.query([Term('next', Var('_'))], kb=copyKb).keys())
+            nextPlayerKnowledge = self.query([Term('knows', self._player, \
+                Term('step', Constant(self._step+1)), Var('_'))], kb=copyKb).keys()
+
+            potentialWorld = World(self._engine, self._baseModel, self._step + 1,\
+                seqProb*self._prob, set(nextState).union(nextPlayerKnowledge), self._player, tokens)
+            if tokens == potentialTokens:
+                successorWorlds.append(potentialWorld)
+
+        return set(successorWorlds)
+
+    # Extract the action out of the knowledge predicate and cumulatively multiply action probabilities to get total sequence probability
+    def _processMoveSequences(self, moveSequences):
+        return list(map(\
+            lambda seq: (set(map(lambda a: Term('knows', a[0].args[0], a[0].args[1], a[0].args[2]),seq)), \
+                reduce(mul, map(lambda a: a[1], seq))), moveSequences))
+
+    def _normaliseProbabilities(self, world):
         pass
 
-    def query(self, query):
-        pass
+    def query(self, queries, kb=None):
+        if kb is None:
+            copyDb = self.copy()
+        else:
+            copyDb = kb.extend()
+        for query in queries:
+            copyDb += Term('query', query)
+        return get_evaluatable().create_from(copyDb).evaluate()
 
-    def _relevantGround(self, queries, database):
-        return self._engine.ground_all(database, queries=queries)
+    def queryFuture(self, queries):
+        copyKb = self.copy()
+        copyKb += Clause(Term('knows', Var('X'), Var('Y'), Var('Z')), Term('knowsMove', Var('X'), Var('Y'), Var('Z')))
+        return self.query(queries, kb=copyKb)
+
+    def getLegalMoves(self, playerOfLegalMove):
+        return set(map(lambda a: a.args[2],
+            self.query([Term('knows', self._player, Term('step', Constant(self._step)), Term('legal', playerOfLegalMove, Var('_')))]).keys()\
+        ))
+        
+
+    def copy(self):
+        return self._kb.extend()
